732400a381407cc71ced8adc833b1671c865ce12
jb <bodysplash@gmail.com>
2013-11-02 09:00:01 +0100

    solution qui fonctionne

--- src/Monnayeur.groovy
+++ src/Monnayeur.groovy
@@ -1,42 +1,47 @@
 class Monnayeur {
 
     def change(cents) {
-        def résultat = Pieces.values().inject([], {
-            résultat, piece ->
-                résultat << découpe(cents, piece)
-                résultat
-        })
-        résultat.removeAll([:])
-        résultat
-    }
-
-    private découpe(cents, piece) {
-        if (cents >= piece.valeur) {
-            def combinaison = [(piece.toString()): piece.vers(cents)]
-            if (cents % piece.valeur > 0) {
-                if (piece == Pieces.NICKELS) {
-                    combinaison << [PENNIES: cents % piece.valeur]
-                }
-            }
-            return combinaison
+        Pieces.values().toList().reverse().inject([[combinaison: [:], reste: cents]]) {
+            résultat, pièce ->
+                découpe(pièce, résultat)
+        }.collect {
+            it.combinaison
         }
-        return [:]
+
+
     }
 
-
+    def découpe(pièce, possibilités) {
+        possibilités.collectMany([], {
+            if (pièce.vers(it.reste) > 0) {
+                def combinatoirs = []
+                if (pièce != Pieces.PENNIES) {
+                    (1..pièce.vers(it.reste)).each {
+                        multiple ->
+                            combinatoirs << [combinaison: it.combinaison.clone() << [(pièce.toString()): multiple], reste: it.reste - multiple * pièce.valeur]
+                    }
+                    combinatoirs << [combinaison: it.combinaison.clone(), reste: it.reste]
+                } else {
+                    combinatoirs << [combinaison:  it.combinaison.clone() << [(pièce.toString()) : it.reste], reste: 0]
+                }
+                return combinatoirs
+            }
+            return [it]
+        })
+    }
 
     private enum Pieces {
         PENNIES(1), NICKELS(5), DIMES(10), QUARTERS(25)
-
 
         Pieces(int valeur) {
             this.valeur = valeur
         }
 
-        public vers(def cents) {
+        public vers(cents) {
             (cents / valeur) as int
         }
 
-        private final def valeur;
+
+        public final def valeur;
     }
 }
--- test/MonnayeurTest.groovy
+++ test/MonnayeurTest.groovy
@@ -13,25 +13,56 @@ class MonnayeurTest {
     public void peutGérerLesPennies() {
         def possibilités = change(1)
         assert possibilités.size() == 1
-        doitContenir(possibilités, [PENNIES : 1])
-        doitContenir(change(2), [PENNIES : 2])
+        doitContenir(possibilités, [PENNIES: 1])
+        doitContenir(change(2), [PENNIES: 2])
     }
 
     @Test
-    public void peutGérerLesNickels() {
-        doitContenir(change(5), [NICKELS : 1])
-        doitContenir(change(10), [NICKELS : 2])
+    public void peutDonnerLesNickels() {
+        doitContenir(change(5), [NICKELS: 1])
     }
 
     @Test
-    public void peutGérerLesAutresCasDeBase() {
-        doitContenir(change(10), [DIMES :1])
-        doitContenir(change(25), [QUARTERS :1])
+    public void donneÉgalementLesPlusFaiblesPossibilités() {
+        doitContenir(change(5), [PENNIES: 5])
     }
 
     @Test
-    public void peutRendreLaMonnaieSurPlusieursPièces() {
-        doitContenir(change(6), [PENNIES: 1, NICKELS: 1])
+    public void peutDonnerLaMonnaieSurPlusieursPièces() {
+        doitContenir(change(6), [NICKELS: 1, PENNIES: 1])
+    }
+
+    @Test
+    public void peutProposerPlusieursCombinatoiresQuandLaPieceRentrePlusieursFois() {
+        doitContenir(change(10), [NICKELS: 1, PENNIES: 5])
+    }
+
+    @Test
+    public void neGénèrePasTropDePossibilités() {
+        assert change(2).size() == 1
+    }
+
+    @Test
+    public void peutGérerLesDimes() {
+        doitContenir(change(10), [DIMES: 1])
+    }
+
+    @Test
+    public void peutGérerLesQuarters() {
+        doitContenir(change(25), [QUARTERS: 1])
+    }
+
+    @Test
+    public void peutGérerLeCasAttenduDuKata() {
+        def possibilités = change(15)
+
+        assert possibilités.size() == 6
+        doitContenir(possibilités, [DIMES: 1, NICKELS: 1])
+        doitContenir(possibilités, [DIMES: 1, PENNIES: 5])
+        doitContenir(possibilités, [NICKELS: 3])
+        doitContenir(possibilités, [NICKELS: 2, PENNIES: 5])
+        doitContenir(possibilités, [NICKELS: 1, PENNIES: 10])
+        doitContenir(possibilités, [PENNIES: 15])
     }
 
     private change(int cents) {
@@ -39,6 +70,7 @@ class MonnayeurTest {
     }
 
     void doitContenir(def change, def possibilité) {
-        assert change.any { it == possibilité}
+        println("comparaison : $change")
+        assert change.any { it == possibilité }
     }
 }
